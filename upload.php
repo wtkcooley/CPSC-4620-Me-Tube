35c8c81fcd0cc21715fbc7c984a5f80a933a2a0a
tealblu <ckhartsell@gmail.com>
2022-04-13 16:53:51 -0400

    final commit of the day: 5.13.22

--- upload.php
+++ upload.php
@@ -47,6 +47,8 @@ uploadIP INT
 uploadTime datetime
 title varchar(50)
 description varchar(240)
+mediaType varchar(5)            <-- THIS NEEDS TO BE IMPLEMENTED
+mediaExt varchar(5)             <-- THIS NEEDS TO BE IMPLEMENTED
 path varchar(100) */
 
 // save DB info
