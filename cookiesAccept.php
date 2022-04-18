dbae2b7257b6578f6e36d2d8dae7b25f8ad4d674
Chase Guynup <chasepg0821@gmail.com>
2022-04-18 10:31:06 -0400

    new stuffs

---
+++ cookiesAccept.php
@@ -0,0 +1,30 @@
+<?php
+
+header( "refresh:15; url=/~cguynup/metube/browse.php" );
+
+?>
+<!DOCTYPE html>
+    <html>
+    <head>
+        <title>MeTube</title>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+        <!--Import jquery-->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <!--Import materialize.js-->
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </head>
+
+    <body style="background-image: url('/~cguynup/metube/images/main_bg.jpg');background-repeat: no-repeat">
+        <div class="z-depth-5" style="color: #37474f; background-color: white; padding: 50px; border-radius: 15px 50px; margin: 0; position: absolute; top: 50%; left: 50%; -ms-transform: translate(-50%, -50%); transform: translate(-50%, -50%); text-align: center;">
+            <h4>Welcome to MeTube!</h4><br>
+            <p>This site uses cookies. You will be redirected to the home page in 15 seconds.</p>
+            <p>By entering the homepage you agree to the use of cookies!</p>
+            <a href="/~cguynup/metube/browse.php"> Click here if it did not load...</a>
+        </div>
+    </body>
+    </html>
