f39d3381884f937e9cfe95fa383ec290e06d670b
Charles "Blue" Hartsell <98487762+tealblu@users.noreply.github.com>
2022-04-20 10:44:57 -0400

    added playlist feature

--- view-media.php
+++ view-media.php
@@ -197,6 +197,17 @@
             echo $path; 
             ?> download><i class="material-icons left">download</i>Download</a>
         </div>
+
+        <div class="row" style="padding: 10px;">
+            <!--PLAYLIST-->
+            <!--<a class="waves-effect waves-light btn "><i class="material-icons left">playlist_add</i>Add to playlist</a>-->
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+                <input name="playlistName" type="text"/>
+                <label>Add To Playlist</label>
+                <button class="btn waves-effect waves-light" type="submit" name="action">Add to Playlist</button>
+            </form>
+        </div>
+
         <div class="row">
             <form class="col s12" action="/~cguynup/metube/submitComment.php" method="POST">
                 <div class="row">
