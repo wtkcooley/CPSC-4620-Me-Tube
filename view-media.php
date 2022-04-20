2cb5e30ee8ee8d87107d8d4c63982e4685126a67
Charles "Blue" Hartsell <98487762+tealblu@users.noreply.github.com>
2022-04-20 10:50:10 -0400

    added playlist feature

--- view-media.php
+++ view-media.php
@@ -202,8 +202,7 @@
             <!--PLAYLIST-->
             <!--<a class="waves-effect waves-light btn "><i class="material-icons left">playlist_add</i>Add to playlist</a>-->
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
-                <input name="playlistName" id="playlistName" type="text">
-                <label for="playlistName">Playlist name</label>
+                <input name="playlistName" id="playlistName" type="text" value="Enter playlist name...">
                 <button class="btn waves-effect waves-light" type="submit" name="action">Add to Playlist</button>
             </form>
         </div>
